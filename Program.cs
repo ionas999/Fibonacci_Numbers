ffc2bc5963af0379bf8c063bebbe7b38d80821dd
ionas_999 <104217036+ionas999@users.noreply.github.com>
2023-03-21 18:23:25 +0100

    changing error

--- Program.cs
+++ Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿const int AMOUNT = 38;
+
+int[] fibonaci = new int[AMOUNT + 1];
+fibonaci[0] = 0;
+fibonaci[1] = 1;
+
+for (int i = 2; i < fibonaci.Length; i++)
+{
+    fibonaci[i] = fibonaci[i - 1] + fibonaci[i - 2];
+}
+
+Console.WriteLine(fibonaci[fibonaci.Length - 1]);
